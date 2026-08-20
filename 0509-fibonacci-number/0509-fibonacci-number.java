c8d887937b768c529e4607e05ae875a916644bf0
Som <somprakash5299@gmail.com>
2026-08-20 11:25:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,11 @@
 import java.util.*;
 class Solution {
     public int fib(int n) {
-        
-        
-        int a=0;
-        int b=1;
-        for(int i=0;i<n;i++){
-        
-        int temp=a;
-        a=b;
-        b=  temp+b;
-    }
-    return a;
+       if(n==0){
+        return 0;
+     }if(n==1){
+        return 1;
+     }
+     return fib(n-1)+fib(n-1);
 }
 }
