7233c5cacfa26240fdfc2e55792678339ca857ff
Som <somprakash5299@gmail.com>
2026-08-20 11:18:16 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.95%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int fib(int n) {
+        
+        
+        int a=0;
+        int b=1;
+        for(int i=0;i<n;i++){
+        
+        int temp=a;
+        a=b;
+        b=  temp+b;
+    }
+    return a;
+}
+}
